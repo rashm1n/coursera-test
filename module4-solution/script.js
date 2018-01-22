31688a4eeb77db088d699124e90e2367ddb7b038
ravindurashmin <rashmin.ravindu@gmail.com>
2018-01-22 14:09:29 +0530

    accomplished the requirements !

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,3 +1,38 @@
+
+(function(){
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for( var i in names){
+      var firstLetter = (names[i].toLowerCase().charAt(0));
+      if (firstLetter=='j'){
+            (function(window){
+                var speakWord = "Hello";
+                var helloSpeaker = new Object();
+                helloSpeaker.speak = function(name) {
+                  console.log(speakWord + " " + name);
+                }
+                window.helloSpeaker=helloSpeaker;
+            })(window);
+            helloSpeaker.speak(names[i]);
+      }else{
+            (function(window){
+                var speakWord = "Good Bye"
+                var byeSpeaker = new Object();
+                byeSpeaker.speak = function (name) {
+                  console.log(speakWord + " " + name);
+                }
+                window.byeSpeaker= byeSpeaker;
+            })(window);
+            byeSpeaker.speak(names[i]); 
+      }
+    }  
+})();
+
+
+
+
+
+
+
 // *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -20,32 +55,50 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
-
 */
-
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
-  // STEP 11:
+ // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+      // See Lecture 52, part 2 - done
+      // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+      // the "speak" method and which you will expose to the global context
+      // See Lecture 52, part 1
+      // var helloSpeaker =
+      // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+      // STEP 4: Rewrite the 'speak' function such that it is attached to the
+      // helloSpeaker object instead of being a standalone function.
+      // See Lecture 52, part 2
+      // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+      // 'helloSpeaker' on the global scope as well.
+      // See Lecture 52, part 2
+      // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+      // xxxx.xxxx = helloSpeaker;
+// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+      // the "speak" method and which you will expose to the global context
+      // See Lecture 52, part 1
+      // var byeSpeaker =
+      // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+      // STEP 8: Rewrite the 'speak' function such that it is attached to the
+      // byeSpeaker object instead of being a standalone function.
+      // See Lecture 52, part 2
+      // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+      // 'byeSpeaker' on the global scope as well.
+      // xxxx.xxxx = byeSpeaker;
+// STEP 11:
   // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
+  // Use the string object's 'charAt' fun-ction. Since we are looking for
   // names that start with either upper case or lower case 'J'/'j', call
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
@@ -57,9 +110,5 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
-  }
-}
+
+
